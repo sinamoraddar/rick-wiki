caa3c63d8b9f48685c98a1ecc5194add4f32f7db
Sina Moraddar <sinamoraddar@gmail.com>
2023-06-17 09:21:49 +0330

    use next image

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from "./Card.module.scss";
+import Image from "next/image";
 
 const Card = ({ info }: { info: any }) => {
   return (
     <div className={styles.Card}>
-      {info.image !== "" && <img src={info.image} alt={info.name} />}
+      {info.image !== "" && (
+        <Image src={info.image} alt={info.name} width={300} height={300} />
+      )}
       <div className={styles.Info}>
         <h3 className={styles.Name}>{info.name}</h3>
         <p className={`${styles.Status} ${styles.Tag}`}>{info.status}</p>
